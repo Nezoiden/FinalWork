a147fd4eeda4d159c8b54e8493081168b546241b
Sergey <xo4ywka@yandex.ru>
2022-08-10 00:16:25 +0300

    написал программу

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+string[] array = new string[5];
+Console.WriteLine("Заполните массив: ");
+for (int i = 0; i < 5; i++)
+{
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.Write("[" + string.Join(",", array) + "]");
+Console.Write(" -> [ ");
+
+for (int i = 0; i < 5; i++)
+{
+
+
+    if (array[i].Length <= 3)
+        Console.Write($"{array[i]},");
+
+
+}
+
+Console.Write("\b]");
